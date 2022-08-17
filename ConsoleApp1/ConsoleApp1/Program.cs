d18e756d0c0473bba286a119bfc083639d8327e0
hsho1017 <hsho1017@naver.com>
2022-08-18 08:59:13 +0900

    aaa

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string name = "너굴맨";
+            int level = 2;
+            int hp = 10;
+            int maxHP = 20;
+            float exp = 0.1f;
+            float maxExp = 1.0f;
+            PrintCharacter();
+
+            private static void PrintCharacter(string name, int level, int hp, int maxHP, float exp, float maxExp)
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
